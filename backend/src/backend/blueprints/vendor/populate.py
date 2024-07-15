3621c0271c2a9e52ec9c81b817617df0c1db7680
SmokierLemur51 <ldl6147@gmail.com>
2024-07-14 22:04:46 -0400

    development

--- backend/src/backend/blueprints/vendor/populate.py
+++ backend/src/backend/blueprints/vendor/populate.py
@@ -7,13 +7,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 from ...models.models import Vendor
 from ...extensions import fbcrypt
+from ...toolbox import helpers
+
+values = ['cold', 'water', 'chirp', 'sand', 'blues', 'math', 
+          'rave', 'bottle', 'paper', 'note', 'clean', 'charge']
 
 def vendors(db: SQLAlchemy) -> None:
     vendors = [
         Vendor(
+            private_username="cocainemaster", 
+            public_username="pabloescobar",
+            secret_phrase="cocaine cookies coochie",
+            hashed_pw=fbcrypt.generate_password_hash(helpers.convert_list_string(helpers.pick_random_choices(values, 3)))
+        ),
+        Vendor(
+            private_username="darkwizard", 
+            public_username="wizardman",
+            secret_phrase="wizards watermelons water",
+            hashed_pw=fbcrypt.generate_password_hash(helpers.convert_list_string(helpers.pick_random_choices(values, 3)))
+        ),
+        Vendor(
+            private_username="misspotion", 
+            public_username="thepotionqueen",
+            secret_phrase="potions parties powder",
+            hashed_pw=fbcrypt.generate_password_hash(helpers.convert_list_string(helpers.pick_random_choices(values, 3)))
+        ),
+        Vendor(
             private_username="", 
             public_username="",
             secret_phrase="",
-            hashed_pw=fbcrypt.generate_password_hash()
+            hashed_pw=fbcrypt.generate_password_hash(helpers.convert_list_string(helpers.pick_random_choices(values, 3)))
+        ),
+        Vendor(
+            private_username="", 
+            public_username="",
+            secret_phrase="",
+            hashed_pw=fbcrypt.generate_password_hash(helpers.convert_list_string(helpers.pick_random_choices(values, 3)))
         ),
 ]
---
+++ backend/src/backend/toolbox/helpers.py
@@ -0,0 +1,23 @@
+import random
+from typing import List
+
+def pick_random_choices(l: List, count: int) -> List:
+    """
+    Return random item/items from provided list.
+
+    Parameters:
+    l (List): List of items you want to sort through, can be of any type.
+    count (int): Number of items you want to return.
+
+    Returns:
+    List containing (count parameter) number of items from original list
+    """
+    output = []
+    for i in range(count):
+        output.append(random.choice(l))
+    return output
+
+
+def convert_list_string(l: List) -> str:
+    return "".join(v for v in l)
+
